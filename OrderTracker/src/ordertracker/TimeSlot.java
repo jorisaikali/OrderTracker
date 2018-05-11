8a117bdbb394eb4d4ce67b31e45fdc2e6029a8dc
jorisaikali <jori.saikali@hotmail.com>
2018-05-11 01:22:14 -0400

    Changed from eclipse to netbeans, added graphics (in progress)

---
+++ OrderTracker/src/ordertracker/TimeSlot.java
@@ -0,0 +1,46 @@
+package ordertracker;
+
+public class TimeSlot {
+
+    private final String time;
+    private String customerName;
+    private final String meridiem;
+	
+    public TimeSlot(String t, String cN, String m) {
+        time = t;
+	customerName = cN;
+	meridiem = m;
+    }
+	
+    public TimeSlot(String t, String m) {
+    	time = t;
+	customerName = null;
+	meridiem = m;
+    }
+	
+    public String GetTime() { return time; }
+    public String GetCustomerName() { return customerName; }
+    public String GetMeridiem() { return meridiem; }
+	
+    public void PrintTimeSlot() {
+	System.out.print(time + meridiem + ", " + customerName);
+    }
+	
+    public String WriteTimeSlot() {
+	String cn = "null";
+	
+	if (customerName != null) {
+            cn = customerName.replace(' ', ';');
+	}
+		
+	return time + " " + meridiem + " " + cn + "\n";
+    }
+	
+    public void AddCustomer(String name) {
+	customerName = name;
+    }
+	
+    public void RemoveCustomer() {
+	customerName = null;
+    }
+}
